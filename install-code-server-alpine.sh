975863352586c5ce78700afc9bb005b9ac12374a
Renan Lira <renan.liraj@gmail.com>
2023-06-29 20:26:49 -0300

    Update install-code-server-alpine.sh

--- install-code-server-alpine.sh
+++ install-code-server-alpine.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Install code server
-set -x
+apk update
 apk upgrade
-apk --no-cache add npm alpine-sdk libstdc++ libc6-compat
-npm config set python python3
-git clone --recursive https://github.com/cdr/code-server.git
-cd code-server
-yarn global add code-server
+apk --no-cache add alpine-sdk libstdc++ libc6-compat
+apk add nodejs npm --repository http://dl-cdn.alpinelinux.org/alpine/v3.16/main
+curl -fsSL https://code-server.dev/install.sh | sh
 
-ENV PASSWORD=changeme
-ENTRYPOINT code-server --bind-addr 0:8443
+# npm config set python python3
+# git clone --recursive https://github.com/cdr/code-server.git
+# cd code-server
+# yarn global add code-server
+
+# ENV PASSWORD=changeme
+# ENTRYPOINT code-server --bind-addr 0:8443

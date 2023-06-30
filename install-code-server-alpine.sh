d942b31f94307e8525015b6a5f4d3790b4640e14
Renan Lira <renan.liraj@gmail.com>
2023-06-30 01:50:34 -0300

    Update install-code-server-alpine.sh

--- install-code-server-alpine.sh
+++ install-code-server-alpine.sh
@@ -3,7 +3,7 @@
 # Install code server
 apk update
 apk upgrade
-apk add alpine-sdk gcompat libstdc++ libc6-compat build-base bash python3 nodejs-current npm yarn argon2
+apk add alpine-sdk gcompat libstdc++ libc6-compat build-base bash python3 nodejs npm yarn argon2
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-armhf
 npm config set python python3
 npm install --global code-server --unsafe-perm

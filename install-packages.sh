9adc57db4ef774f65588fbdc8b71789ace1301ce
Renan Lira <renanlira@bluems.com>
2024-05-20 12:45:05 -0300

    chore: Update install-packages.sh to set Bash as default shell, install additional packages, and configure extras

--- install-packages.sh
+++ install-packages.sh
@@ -15,7 +15,10 @@ if [ $OS != "Linux" ] && [ $OS != "Darwin" ]; then
 fi
 
 ## Set Bash as default shell
-chsh -s /bin/bash $USER &>/dev/null
+
+## Verify if Bash is the default shell and if it is not, change it
+test $SHELL == "/bin/bash" || sudo chsh -s /bin/bash
+
 if [ $OS == "Linux" ]; then
     BASHFILE=$HOME/.bashrc
 elif [ $OS == "Darwin" ]; then
@@ -32,12 +35,12 @@ if [ $OS == "Linux" ]; then
         INSTALL="sudo dnf install -y"
     fi
 elif [ $OS == "Darwin" ]; then
-        install_homebrew
+        test ! -f /opt/homebrew/bin/brew && install_homebrew
         INSTALL="brew install"
 fi
 
 ## Install initial packages
-$INSTALL neofetch figlet ed jq wget curl git gawk
+$INSTALL neofetch figlet ed jq wget curl git gawk make unzip
 
 ## Configure vim
 $INSTALL vim
@@ -62,11 +65,6 @@ git clone --depth=1 https://github.com/therenanlira/bash-it-themes.git ~/.bash-i
 sed -i "" "s/export BASH_IT_THEME=.*/\export BASH_IT_THEME=new-sushu/g" $BASHFILE
 source $BASHFILE
 
-## Extras Bash configurations
-rm $HOME/.extras &>/dev/null \
-&& curl -o $HOME/.extras https://raw.githubusercontent.com/therenanlira/dev-environment/main/extras \
-&& echo -e "\n# Load extras\ntest -f \$HOME/.extras && source \$HOME/.extras" >> $BASHFILE
-
 ## Install programming languages
 $INSTALL nodejs npm python3 pipx golang
 
@@ -74,14 +72,81 @@ $INSTALL nodejs npm python3 pipx golang
 test $OS == "Darwin" && xcode-select --install
 
 ## Install TLDR
-npm install -g tldr
+if [ $OS == "Linux" ]; then
+    sudo npm install -g tldr
+elif [ $OS == "Darwin" ]; then
+    npm install -g tldr
+fi
 
-## Install Cloud tools
-$INSTALL awscli azure-cli terraform
+## Install FZF
+if [ $OS == "Linux" ]; then
+    $INSTALL fzf
+elif [ $OS == "Darwin" ]; then
+    $INSTALL fzf
+    $(brew --prefix)/opt/fzf/install
+fi
+
+## Install Terraform
+$INSTALL terraform
+
+## Install AWS CLI
+if [ $OS == "Linux" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    rm -rf awscliv2.zip aws
+elif [ $OS == "Darwin" ]; then
+    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+    sudo installer -pkg AWSCLIV2.pkg -target /
+    rm -rf AWSCLIV2.pkg
+fi
+
+## Install Azure CLI
+if [ $OS == "Linux" ]; then
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+elif [ $OS == "Darwin" ]; then
+    $INSTALL azure-cli
+fi
+
+## Install Google Cloud CLI
+if [ $OS == "Linux" ]; then
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+    sudo apt update
+    $INSTALL google-cloud-sdk
+elif [ $OS == "Darwin" ]; then
+    $INSTALL --cask google-cloud-sdk
+fi
 
 ## Install Kubernetes tools
-$INSTALL kubectl kubectx k9s kubent bash-completion fzf helm k9s
-kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
+if [ $OS == "Linux" ]; then
+    $INSTALL kubectl
+    source <(kubectl completion bash)
+
+    sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+    sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+    sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+    sh -c "$(curl -sSL https://git.io/install-kubent)"
+
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    rm get_helm.sh
+
+    curl -sS https://webinstall.dev/k9s | bash
+elif [ $OS == "Darwin" ]; then
+    $INSTALL kubectl
+    $INSTALL bash-completion
+    kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
+
+    $INSTALL kubectl kubectx
+    $INSTALL kubent
+
+    $INSTALL helm
+    
+    $INSTALL k9s
+fi
 
 ### Install krew
 (
@@ -97,13 +162,22 @@ kubectl completion bash > $(brew --prefix)/etc/bash_completion.d/kubectl
 ### Install kubectl-node-shell
 kubectl krew install neat
 curl -LO https://github.com/kvaps/kubectl-node-shell/raw/master/kubectl-node_shell
-chmod +x ./kubectl-node_shell
+sudo chown root:root ./kubectl-node_shell
+sudo chmod +x ./kubectl-node_shell
 sudo mv ./kubectl-node_shell /usr/local/bin/kubectl-node_shell
 
 ### Install CMCTL and CFSSL
 if [ $OS == "Linux" ]; then
     CMCTL_VERSION=0.5.0
-    curl -L -s "https://github.com/oleewere/cmctl/releases/download/v${CMCTL_VERSION}/cmctl_${CMCTL_VERSION}_linux_64-bit.tar.gz" | tar -C /usr/bin -xzv cmctl
+    curl -fsSLO "https://github.com/oleewere/cmctl/releases/download/v${CMCTL_VERSION}/cmctl_${CMCTL_VERSION}_linux_64-bit.tar.gz" \ 
+    && sudo tar zxvf cmctl_${CMCTL_VERSION}_linux_64-bit.tar.gz -C /usr/local/bin cmctl
+    rm cmctl_${CMCTL_VERSION}_linux_64-bit.tar.gz
+
+    CFSSL_VERSION=1.6.5
+    curl -fsSLO "https://github.com/cloudflare/cfssl/releases/download/v${CFSSL_VERSION}/cfssl-bundle_${CFSSL_VERSION}_linux_amd64" \
+    && sudo mv cfssl-bundle_${CFSSL_VERSION}_linux_amd64 /usr/local/bin/cfssl
+    sudo chown root:root /usr/local/bin/cfssl
+    sudo chmod +x /usr/local/bin/cfssl
 elif [ $OS == "Darwin" ]; then
     brew tap oleewere/repo
     $INSTALL cmctl
@@ -111,15 +185,21 @@ elif [ $OS == "Darwin" ]; then
 fi
 
 ## Install e1s
-E1S_VERSION=1.0.34
-E1S_URL="https://github.com/keidarcy/e1s/releases/download/v$E1S_VERSION/e1s_$E1S_VERSION_$E1S_OS.tar.gz"
-test $OS == "Linux" && E1S_OS="linux_amd64" || E1S_OS="darwin_all"
-
 if [ $OS == "Linux" ]; then
-    curl -L -s $E1S_URL | tar -C /usr/bin -xzv e1s
+    E1S_VERSION=1.0.34
+    E1S_OS=linux_amd64
+    E1S_URL=https://github.com/keidarcy/e1s/releases/download/v$E1S_VERSION/e1s_$E1S_VERSION\_$E1S_OS.tar.gz
+
+    curl -fsSLO $E1S_URL \
+    && sudo tar zxvf e1s_$E1S_VERSION\_$E1S_OS.tar.gz -C /usr/local/bin e1s
+    sudo chown root:root /usr/local/bin/e1s
+    sudo chmod +x /usr/local/bin/e1s
+    rm e1s_$E1S_VERSION\_$E1S_OS.tar.gz
 elif [ $OS == "Darwin" ]; then
-    curl -L -s $E1S_URL | tar -C /usr/local/bin -xzv e1s
+    $INSTALL e1s
 fi
+
+## TODO continue from here
 
 ## Install Docker
 read -p "Install Docker? [y/N] " yn
@@ -154,3 +234,8 @@ case $yn in
     [Yy] )  $INSTALL postman;;
     [Nn]* ) ;;
 esac
+
+## Extras Bash configurations
+rm $HOME/.extras &>/dev/null \
+&& curl -o $HOME/.extras https://raw.githubusercontent.com/therenanlira/dev-environment/main/extras \
+&& echo -e "\n# Load extras\ntest -f \$HOME/.extras && source \$HOME/.extras" >> $BASHFILE
